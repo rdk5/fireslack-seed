8d8c04cd2ee3dd831d3aa929bcafc2b8123d01d6
Ricky Kharawala <rdk5@njit.edu>
2016-01-31 17:38:32 -0500

    Reverted back to start

--- app/app.js
+++ app/app.js
@@ -22,39 +22,13 @@ angular
       })
       .state('login', {
         url: '/login',
-        controller: 'AuthCtrl as authCtrl',
         templateUrl: 'auth/login.html'
-        resolve: {
-          requireNoAuth: function($state, Auth){
-            return Auth.$requireAuth().then(function(auth){
-              $state.go('home');
-            }, function(error){
-              return;
-            });
-          }
-        }
       })
       .state('register', {
         url: '/register',
-        controller: 'AuthCtrl as authCtrl',
         templateUrl: 'auth/register.html'
-      })
-      .state('profile',{
-        url: '/profile',
-        resolve: {
-          auth: function($state, Users, Auth){
-            return Auth.$requireAuth().catch(function(){
-              $state.go('home');
-            });
-          },
-          profile: function(Users, Auth){
-            return Auth.$requireAuth().then(function(auth){
-              return Users.getProfile(auth.uid).$loaded();
-            });
-          }
-        }
-      })
+      });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'fireslack-rdk5.firebaseIO.com');
+  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
--- app/auth/profile.controller.js
+++
@@ -1,4 +0,0 @@
-angular.module('angularfireSlackApp')
-.controller('ProfileCtrl', function($state, md5, auth, profile){
-  var ProfileCtrl = this;
-});
